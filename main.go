03e20d60c796d7bd2e4f9514f009a98221be9d27
sensvision <56294095+sensvision@users.noreply.github.com>
2020-12-30 09:46:29 +0400

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		fmt.Fprintf(w, "Hello !!!")
 	})
 	http.ListenAndServe(":80", nil)
 }
